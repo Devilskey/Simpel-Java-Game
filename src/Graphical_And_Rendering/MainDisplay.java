90f68294637547126ef03d8ae2533538d692cb57
Tamás B. Nemes <tb.nemes@gmail.com>
2024-05-21 14:32:58 +0200

    reverting

--- src/Graphical_And_Rendering/MainDisplay.java
+++ src/Graphical_And_Rendering/MainDisplay.java
@@ -13,21 +13,22 @@ import java.awt.image.ImageObserver;
 public class MainDisplay extends Canvas {
     public KeyboardHandler key = new KeyboardHandler();
 
-    public MainDisplay(){
+    public MainDisplay() {
         addKeyListener(key);
     }
+
     long TimeLastFrame = 0;
 
-    public void UpdateDisplay () {
+    public void UpdateDisplay() {
         SceneManager.SceneLoaded.Update();
     }
 
-    public void Render(){
-        if(TimeLastFrame == 0)
+    public void Render() {
+        if (TimeLastFrame == 0)
             TimeLastFrame = System.nanoTime();
         RenderSceneData SceneData = SceneManager.SceneLoaded.RenderdScene();
         BufferStrategy Buffer = this.getBufferStrategy();
-        if(Buffer == null){
+        if (Buffer == null) {
             createBufferStrategy(3);
             return;
         }
@@ -36,7 +37,7 @@ public class MainDisplay extends Canvas {
         SceneData.RenderEntities(graphics);
         Buffer.show();
 
-        GameData.fps = (int) (1000000000.0  / ( System.nanoTime() - TimeLastFrame));
+        GameData.fps = (int) (1000000000.0 / (System.nanoTime() - TimeLastFrame));
         TimeLastFrame = System.nanoTime();
 
     }
--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,39 @@ import objects.SizeObjects.Vector2;
 
 import javax.swing.*;
 
-
 public class Main {
-    private static final String title = "Hello world";
-    private static final JFrame MainWindow = new JFrame();
-    private static MainDisplay display;
-    private static final Vector2 ScreenSize = new Vector2(500, 500);
+        private static final String title = "Hello world";
+        private static final JFrame MainWindow = new JFrame();
+        private static MainDisplay display;
+        private static final Vector2 ScreenSize = new Vector2(500, 500);
 
-    public static void main(String[] args) {
-             DebugSettings.StartDebugWindow();
+        public static void main(String[] args) {
+                DebugSettings.StartDebugWindow();
 
-            SceneManager.SwitchLoadedScene(new MainScene());
+                SceneManager.SwitchLoadedScene(new MainScene());
 
-            display = new MainDisplay();
+                display = new MainDisplay();
 
-            MainWindow.add(display);
-            MainWindow.pack();
-            MainWindow.setFocusable(true);
-            MainWindow.setFocusTraversalKeysEnabled(false);
-            MainWindow.setTitle(title + " Fps = " + GameData.fps);
-            MainWindow.setSize((int) ScreenSize.GetX(), (int) ScreenSize.GetY());
-            MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            MainWindow.setLocationRelativeTo(null);
-            MainWindow.setResizable(true);
-            MainWindow.setVisible(true);
+                MainWindow.add(display);
+                MainWindow.pack();
+                MainWindow.setFocusable(true);
+                MainWindow.setFocusTraversalKeysEnabled(false);
+                MainWindow.setTitle(title + " Fps = " + GameData.fps);
+                MainWindow.setSize((int) ScreenSize.GetX(), (int) ScreenSize.GetY());
+                MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                MainWindow.setLocationRelativeTo(null);
+                MainWindow.setResizable(true);
+                MainWindow.setVisible(true);
 
-            while (MainWindow.isVisible()) {
-                GameLoop();
-                KeyboardHandler.CheckIfButtonMapIspressed();
-                KeyboardHandler.CheckButtonState();
-            }
-    }
+                while (MainWindow.isVisible()) {
+                        GameLoop();
+                        KeyboardHandler.CheckIfButtonMapIspressed();
+                        KeyboardHandler.CheckButtonState();
+                }
+        }
 
-    static void GameLoop(){
-        display.UpdateDisplay();
-        display.Render();
-    }
+        static void GameLoop() {
+                display.UpdateDisplay();
+                display.Render();
+        }
 }
